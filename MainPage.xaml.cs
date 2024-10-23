69f905502016d1cd41aa268de596c1a9c0ef0dd7
Hamza Almahmoud <101905387+Hamza7m@users.noreply.github.com>
2024-10-23 21:54:37 +0300

    ilk versyon

---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0; 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            PerformOperation((x, y) => x + y);
+        }
+
+        private void OnSubtractClicked(object sender, EventArgs e)
+        {
+            PerformOperation((x, y) => x - y);
+        }
+        private void OnMultiplyClicked(object sender, EventArgs e)
+        {
+            PerformOperation((x, y) => x * y);
+        }
+
+        private void OnDivideClicked(object sender, EventArgs e)
+        {
+            PerformOperation((x, y) => x / y);
+        }
+        private void PerformOperation(Func<double, double, double> operation)
+        {
+            if (double.TryParse(input1.Text, out double num1) && double.TryParse(input2.Text, out double num2))
+            {
+                double result = operation(num1, num2);
+                resultLabel.Text = $"Sonuç: {result}";
+            }
+            else
+            {
+                resultLabel.Text = "Geçersiz giriş!";
+            }
+        }
+
+    }
+
+}
+
+
